f111f556e8217d7e0a7e89ff8fb85f1fe2c5d6a1
Jair Eduardo Tellez Romero <jairtellezromero9678@gmail.com>
2022-11-08 17:05:24 -0600

    Clean project, add router-dom, and install axios, using npm

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <>
+            <div className='main-container'>
+                <p className='header-text'>Rick and Morty</p>
+            </div>
+        </>
+    )
+}
+
+export default App
